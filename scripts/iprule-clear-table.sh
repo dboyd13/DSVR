8771b6de07d7efdb75ffc5290887f932734c90cd
dboyd13 <dboyd13@gmail.com>
2013-06-28 15:11:13 +0800

    Update iprule-clear-table.sh

--- scripts/iprule-clear-table.sh
+++ scripts/iprule-clear-table.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+# DSVR (Domain Specific VPN Router)
+# Copyright 2013 Darran Boyd
+#
+# Licensed under the "Attribution-NonCommercial-ShareAlike" Vizsage
+# Public License (the "License"). You may not use this file except
+# in compliance with the License. Roughly speaking, non-commercial
+# users may share and modify this code, but must give credit and 
+# share improvements. However, for proper details please 
+# read the full License, available at
+#     http://vizsage.com/license/Vizsage-License-BY-NC-SA.html 
+# and the handy reference for understanding the full license at 
+#     http://vizsage.com/license/Vizsage-Deed-BY-NC-SA.html
+#
+# Unless required by applicable law or agreed to in writing, any
+# software distributed under the License is distributed on an 
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+# either express or implied. See the License for the specific 
+# language governing permissions and limitations under the License.
 
 if [ -z "$1" ]
   then
